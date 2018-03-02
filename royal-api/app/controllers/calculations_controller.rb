d532652fd4a49770e8ccb7655375ce75fe4b9006
Eric Loos <ericloos00@gmail.com>
2018-03-01 21:40:30 -0600

    save work

--- royal-api/app/controllers/calculations_controller.rb
+++ royal-api/app/controllers/calculations_controller.rb
@@ -2,7 +2,7 @@ class CalculationsController < ActionController::Base
 
   def index
     #binding.pry
-    @calculations = Calculation.all.as_json(include: [:routes])
+    @calculations = Calculation.all.as_json(include: [:routes, :batches])
     render json: @calculations
   end
 
